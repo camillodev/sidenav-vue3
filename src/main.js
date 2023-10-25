1ec4847587549ea97950b3f170138829958192f7
Rafael Camillo <camillodeveloper@gmail.com>
2023-10-25 17:33:33 -0300

    remove quasar user options

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options';
 
 const app = createApp(App);
-app.use(Quasar, quasarUserOptions);
+app.use(Quasar, {
+  config: {
+    brand: {
+      primary: '#1976d2',
+    }
+  } 
+});
 app.mount('#app');
