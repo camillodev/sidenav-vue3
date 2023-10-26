679028f00cbccb66ca68fc4645092e92e256341e
Rafael Camillo <camillodeveloper@gmail.com>
2023-10-26 07:39:34 -0300

    Revert "remove quasar user options"
    This reverts commit 1ec4847587549ea97950b3f170138829958192f7.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options';
 
 const app = createApp(App);
-app.use(Quasar, {
-  config: {
-    brand: {
-      primary: '#1976d2',
-    }
-  } 
-});
+app.use(Quasar, quasarUserOptions);
 app.mount('#app');
