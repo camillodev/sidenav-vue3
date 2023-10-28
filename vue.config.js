6b228a80f34def0aa05788645210b672747e6933
Rafael Camillo <camillodeveloper@gmail.com>
2023-10-28 11:53:01 -0300

    update vue config to compile sidenav into pure js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: [
-    'quasar'
-  ],
+const { defineConfig } = require('@vue/cli-service');
 
+module.exports = defineConfig({
+  transpileDependencies: ['quasar'],
   pluginOptions: {
     quasar: {
       importStrategy: 'kebab',
-      rtlSupport: true
-    }
-  }
-})
+      rtlSupport: true,
+    },
+  },
+  configureWebpack: {
+    entry: {
+      sidenav: './src/components/SideNav.vue',
+    },
+    output: {
+      filename: '[name].js',
+      libraryExport: 'default',
+    },
+  },
+  productionSourceMap: false,
+});
